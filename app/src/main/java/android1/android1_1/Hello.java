7503df010615601722f32f222993385022b47fca
nekozawa666 <nekozawa@inbox.ru>
2018-05-17 08:10:41 +0500

    Homework2

--- app/src/main/java/android1/android1_1/Hello.java
+++ app/src/main/java/android1/android1_1/Hello.java
@@ -1,7 +1,8 @@
 package android1.android1_1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class Hello extends AppCompatActivity {
 
@@ -9,5 +10,53 @@ public class Hello extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+
+        String instanceState;
+        if (savedInstanceState == null) {
+            instanceState = "Первый запуск!";
+        } else {
+            instanceState = "Повторный запуск";
+        }
+        Toast.makeText(getApplicationContext(), instanceState + getString(R.string.onCreate1), Toast.LENGTH_SHORT).show();
+        }
+        @Override
+    protected void onStart(){
+        super.onStart();
+            Toast.makeText(getApplicationContext(), R.string.onStart, Toast.LENGTH_SHORT).show();
+        }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(getApplicationContext(), R.string.onRestoreInstanceState, Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(getApplicationContext(), R.string.onResume, Toast.LENGTH_SHORT).show();
+        }
+        @Override
+    protected void onPause(){
+        super.onPause();
+            Toast.makeText(getApplicationContext(), R.string.onPause, Toast.LENGTH_SHORT).show();
+        }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Toast.makeText(getApplicationContext(), R.string.onSaveInstanceState, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(getApplicationContext(), R.string.onStop, Toast.LENGTH_SHORT).show();
+        }
+        @Override
+    protected void onDestroy(){
+        super.onDestroy();
+            Toast.makeText(getApplicationContext(), R.string.onDestroy, Toast.LENGTH_SHORT).show();
+        }
+
 }
