df5a2139712f2e8d2e71d572cf57e04e33c40329
nekozawa666 <nekozawa@inbox.ru>
2018-06-02 18:29:59 +0500

    less3 процесс,передача текста во 2 активити, апд.

--- app/src/main/java/android1/android1_1/LifeCircle.java
+++ app/src/main/java/android1/android1_1/LifeCircle.java
@@ -1,5 +1,6 @@
 package android1.android1_1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,7 +8,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class LifeCircle extends AppCompatActivity {
-    private static final String COUNTER_KEY="Counter Key";
+    private static final String COUNTER_KEY="CounterKey";
+    public static final String EXTRA_TEXT="ExtraText";
+    private TextView textView;
     private int increment;
 
     @Override
@@ -15,15 +18,17 @@ public class LifeCircle extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
 
-        final TextView textView=findViewById(R.id.planet);
+        textView = findViewById(R.id.planet);
         final Button button=findViewById(R.id.button1);
 
-        textView.setText("Inc"+increment);
+        textView.setText(R.string.tweet);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                increment++;
-                textView.setText("Inc"+increment);
+                Intent intent=new Intent(LifeCircle.this,SecondActivity.class);
+                //intent.putExtra(Intent.EXTRA_TEXT,textView.getText().toString());
+                intent.putExtra(EXTRA_TEXT,"New day");
+                startActivity(intent);
             }
         });
         }
@@ -31,6 +36,7 @@ public class LifeCircle extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         increment=savedInstanceState.getInt(COUNTER_KEY);
+        textView.setText("inc"+increment);
 
     }
 
